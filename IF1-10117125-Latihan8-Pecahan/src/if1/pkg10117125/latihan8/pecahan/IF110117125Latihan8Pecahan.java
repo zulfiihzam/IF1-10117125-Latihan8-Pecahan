62aae4f4844826581212d652ffc4db6d6fcb4b0b
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:43:42 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan8-Pecahan/src/if1/pkg10117125/latihan8/pecahan/IF110117125Latihan8Pecahan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * @author 
+ */
+package if1.pkg10117125.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF 1
+ * Nim   : 10117125
+ * Deskripsi Program : berisi program yang menampilkan
+ * nilai pecahan
+ */
+public class IF110117125Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " +d);
+    }
+    
+}
